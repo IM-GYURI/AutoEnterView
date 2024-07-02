f926f66a027bbb5064d2cf115aeb817ccb11b7b9
GLORY-JI <gigi716151@gmail.com>
2024-07-02 17:03:18 +0900

    update aonnotation

--- src/main/java/com/ctrls/auto_enter_view/AutoEnterViewApplication.java
+++ src/main/java/com/ctrls/auto_enter_view/AutoEnterViewApplication.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class AutoEnterViewApplication {
 
   public static void main(String[] args) {
+
     SpringApplication.run(AutoEnterViewApplication.class, args);
   }
-
 }
