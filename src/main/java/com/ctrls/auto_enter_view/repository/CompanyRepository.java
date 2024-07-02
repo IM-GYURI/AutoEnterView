b13c8dd60655216322ee74556121b5899ddf8e99
IM-GYURI <kgulr0517@gmail.com>
2024-07-02 23:13:36 +0900

    feat : add existsByEmail and findByEmail

--- src/main/java/com/ctrls/auto_enter_view/repository/CompanyRepository.java
+++ src/main/java/com/ctrls/auto_enter_view/repository/CompanyRepository.java
@@ -1,10 +1,14 @@
 package com.ctrls.auto_enter_view.repository;
 
 import com.ctrls.auto_enter_view.entity.CompanyEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
 
+  boolean existsByEmail(String email);
+
+  Optional<CompanyEntity> findByEmail(String email);
 }
