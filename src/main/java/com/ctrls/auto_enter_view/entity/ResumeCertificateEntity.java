8c22b0249f514227d2d2a6386a8a7eab8a3756a6
Goldbar97 <ehrm1357@gmail.com>
2024-07-02 16:34:30 +0900

    init: project setup

---
+++ src/main/java/com/ctrls/auto_enter_view/entity/ResumeCertificateEntity.java
@@ -0,0 +1,31 @@
+package com.ctrls.auto_enter_view.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "resume_certificate")
+public class ResumeCertificateEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String resumeKey;
+
+  private String certificateName;
+
+  private LocalDate certificateDate;
+}
---
+++ src/main/java/com/ctrls/auto_enter_view/enums/UserRole.java
@@ -0,0 +1,6 @@
+package com.ctrls.auto_enter_view.enums;
+
+public enum UserRole {
+  ROLE_COMPANY,
+  ROLE_CANDIDATE
+}
