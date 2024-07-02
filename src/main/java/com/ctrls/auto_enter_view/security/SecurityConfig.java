28a19b3bbafdd4f2a119169c4e420a2ab14a119b
GLORY-JI <gigi716151@gmail.com>
2024-07-03 00:09:26 +0900

    fix

--- src/main/java/com/ctrls/auto_enter_view/security/SecurityConfig.java
+++ src/main/java/com/ctrls/auto_enter_view/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,7 +23,7 @@ public class SecurityConfig {
         .authorizeHttpRequests(authorize -> authorize
             .anyRequest().permitAll()
         )
-        .csrf((csrf) -> csrf.disable());
+        .csrf(AbstractHttpConfigurer::disable);
 
     return http.build();
   }
