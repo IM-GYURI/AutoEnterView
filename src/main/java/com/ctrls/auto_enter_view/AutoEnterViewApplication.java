2c1674c232e6a509943dc22f6e73583b6f12616b
eunsun <eunsunjj9501@gmail.com>
2024-07-02 20:10:03 +0900

    refactor : Add @EnableJpaAuditing

--- src/main/java/com/ctrls/auto_enter_view/AutoEnterViewApplication.java
+++ src/main/java/com/ctrls/auto_enter_view/AutoEnterViewApplication.java
@@ -2,8 +2,10 @@ package com.ctrls.auto_enter_view;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AutoEnterViewApplication {
 
 	public static void main(String[] args) {
