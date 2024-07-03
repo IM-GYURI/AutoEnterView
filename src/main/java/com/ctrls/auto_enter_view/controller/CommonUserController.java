06ab8439086a3b420aa251f6391ca3db57e48b51
IM-GYURI <kgulr0517@gmail.com>
2024-07-03 17:18:28 +0900

    feat : add @RequestMapping("/common")
    fix : sendTemporaryPassword
    - 성공 시에도 무조건 실패 코드가 뜨는 것을 확인

--- src/main/java/com/ctrls/auto_enter_view/controller/CommonUserController.java
+++ src/main/java/com/ctrls/auto_enter_view/controller/CommonUserController.java
@@ -7,18 +7,19 @@ import com.ctrls.auto_enter_view.dto.common.SignInDto.Response;
 import com.ctrls.auto_enter_view.dto.common.TemporaryPasswordDto;
 import com.ctrls.auto_enter_view.service.CommonUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 인증 없이 접근 가능 - 공통 회원 기능 : 회사, 지원자
  */
 
+@RequestMapping("/common")
 @RestController
 @RequiredArgsConstructor
 public class CommonUserController {
@@ -77,12 +78,11 @@ public class CommonUserController {
     commonUserService.sendTemporaryPassword(temporaryPasswordDto.getEmail(),
         temporaryPasswordDto.getName());
 
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("임시 비밀번호 전송 실패");
-
+    return ResponseEntity.ok("임시 비밀번호 전송 성공");
   }
 
   // 로그인
-  @PostMapping("/common/signin")
+  @PostMapping("/signin")
   public ResponseEntity<?> login(
       @Validated @RequestBody Request request) {
 
