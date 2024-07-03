8112e7af37ab53288f7078b0e3aa435fa4456309
IM-GYURI <kgulr0517@gmail.com>
2024-07-03 21:56:02 +0900

    refactor : change how to handle AbstractException

--- src/main/java/com/ctrls/auto_enter_view/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ctrls/auto_enter_view/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.ctrls.auto_enter_view.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,9 +12,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(AbstractException.class)
-  public ResponseEntity<ErrorResponse> handleCustomException(AbstractException ex) {
-    log.error("handleCustomException", ex);
-    ErrorResponse response = new ErrorResponse(ex.getErrorCode());
-    return new ResponseEntity<>(response, HttpStatus.valueOf(ex.getErrorCode().getStatus()));
+  public ResponseEntity<ErrorResponse> handleCustomException(AbstractException e) {
+    ErrorResponse errorResponse = ErrorResponse.builder()
+        .code(e.getStatusCode())
+        .message(e.getMessage())
+        .build();
+
+    log.error("handleCustomException", e);
+    return new ResponseEntity<>(errorResponse, new HttpHeaders(),
+        HttpStatus.valueOf(e.getStatusCode()));
   }
 }
