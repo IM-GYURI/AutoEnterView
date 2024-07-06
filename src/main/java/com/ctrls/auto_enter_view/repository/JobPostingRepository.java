7573b6edddc7641082bf2485f72be4d4fe70ffa6
Goldbar97 <ehrm1357@gmail.com>
2024-07-06 15:29:12 +0900

    refactor: changed repository id type to string

--- src/main/java/com/ctrls/auto_enter_view/repository/JobPostingRepository.java
+++ src/main/java/com/ctrls/auto_enter_view/repository/JobPostingRepository.java
@@ -3,6 +3,6 @@ package com.ctrls.auto_enter_view.repository;
 import com.ctrls.auto_enter_view.entity.JobPostingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JobPostingRepository extends JpaRepository<JobPostingEntity, Long> {
+public interface JobPostingRepository extends JpaRepository<JobPostingEntity, String> {
 
 }
