d323308522b3208835a1ee02fe36e814c36ff353
eunsun <eunsunjj9501@gmail.com>
2024-07-06 20:22:57 +0900

    feat: Add logout function - 헤더에서 토큰 추출 후 로그아웃 기능 추가 - 추출 시 토큰이 없으면 null 반환

--- src/main/java/com/ctrls/auto_enter_view/controller/CommonUserController.java
+++ src/main/java/com/ctrls/auto_enter_view/controller/CommonUserController.java
@@ -11,8 +11,12 @@ import com.ctrls.auto_enter_view.dto.common.SignInDto.Request;
 import com.ctrls.auto_enter_view.dto.common.SignInDto.Response;
 import com.ctrls.auto_enter_view.dto.common.TemporaryPasswordDto;
 import com.ctrls.auto_enter_view.service.CommonUserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/common")
 @RequiredArgsConstructor
 @RestController
+@Slf4j
 public class CommonUserController {
 
   private final CommonUserService commonUserService;
@@ -96,5 +101,32 @@ public class CommonUserController {
 
     Response response = commonUserService.loginUser(request.getEmail(), request.getPassword());
     return ResponseEntity.ok(response);
+  }
+
+  // 로그 아웃
+  @PostMapping("/signout")
+  public ResponseEntity<String> logout(HttpServletRequest request) {
+
+    String token = getTokenFromRequest(request);
+    if (token != null) {
+      commonUserService.logoutUser(token);
+    } else {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 상태가 아닙니다.");
+    }
+
+    return ResponseEntity.ok("정상적으로 로그아웃 되었습니다.");
+  }
+
+  // 토큰 추출
+  private String getTokenFromRequest(HttpServletRequest request) {
+
+    String token = request.getHeader("Authorization");
+    log.info("header : {}", token);
+
+    if (StringUtils.hasText(token) && token.startsWith("Bearer ")) {
+      return token.substring(7);
+    }
+
+    return null;
   }
 }
