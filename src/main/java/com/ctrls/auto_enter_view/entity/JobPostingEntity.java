520bfa28c5afedbfc2f018d9423868db9b4b714b
GLORY-JI <gigi716151@gmail.com>
2024-07-06 15:32:01 +0900

    Revert "change type"
    This reverts commit 3adcdd1e95fd5d58e3a2e315e2f9a9be21373bf1.

--- src/main/java/com/ctrls/auto_enter_view/entity/JobPostingEntity.java
+++ src/main/java/com/ctrls/auto_enter_view/entity/JobPostingEntity.java
@@ -46,7 +46,7 @@ public class JobPostingEntity extends BaseEntity {
   private String employmentType;
 
   @Column(nullable = false)
-  private Long salary;
+  private String salary;
 
   @Column(nullable = false)
   private String workTime;
