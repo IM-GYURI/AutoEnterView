28e57241f42cd1cb4fc6ce3f2c530f5abe647ed4
GLORY-JI <gigi716151@gmail.com>
2024-07-06 22:30:40 +0900

    feat: add creating job posting step method

--- src/main/java/com/ctrls/auto_enter_view/service/JobPostingStepService.java
+++ src/main/java/com/ctrls/auto_enter_view/service/JobPostingStepService.java
@@ -1,5 +1,11 @@
 package com.ctrls.auto_enter_view.service;
 
+import com.ctrls.auto_enter_view.dto.jobposting.JobPostingDto;
+import com.ctrls.auto_enter_view.entity.JobPostingEntity;
+import com.ctrls.auto_enter_view.entity.JobPostingStepEntity;
+import com.ctrls.auto_enter_view.repository.JobPostingStepRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,5 +14,21 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class JobPostingStepService {
+
+  private final JobPostingStepRepository jobPostingStepRepository;
+
+  public void createJobPostingStep(JobPostingEntity entity, JobPostingDto.Request request) {
+
+    List<String> jobPostingStep = request.getJobPostingStep();
+
+    List<JobPostingStepEntity> entities = jobPostingStep.stream()
+        .map(e -> request.toStepEntity(entity, e))
+        .collect(Collectors.toList());
+
+    jobPostingStepRepository.saveAll(entities);
+
+
+  }
+
 
 }
