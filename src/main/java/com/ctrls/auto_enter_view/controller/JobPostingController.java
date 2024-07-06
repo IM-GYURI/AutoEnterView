c66f5bb237f471ebe2dcd49ec31a89160868aa80
GLORY-JI <gigi716151@gmail.com>
2024-07-06 22:25:51 +0900

    feat: add post method in job posting controller

--- src/main/java/com/ctrls/auto_enter_view/controller/JobPostingController.java
+++ src/main/java/com/ctrls/auto_enter_view/controller/JobPostingController.java
@@ -1,7 +1,15 @@
 package com.ctrls.auto_enter_view.controller;
 
+import com.ctrls.auto_enter_view.dto.jobposting.JobPostingDto;
+import com.ctrls.auto_enter_view.entity.JobPostingEntity;
 import com.ctrls.auto_enter_view.service.JobPostingService;
+import com.ctrls.auto_enter_view.service.JobPostingStepService;
+import com.ctrls.auto_enter_view.service.JobPostingTechStackService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -9,4 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class JobPostingController {
 
   private final JobPostingService jobPostingService;
+
+  private final JobPostingTechStackService jobPostingTechStackService;
+
+  private final JobPostingStepService jobPostingStepService;
+
+  @PostMapping("/companies/{companyKey}/job-postings")
+  public ResponseEntity<String> createJobPosting(@PathVariable String companyKey,
+      @RequestBody JobPostingDto.Request request) {
+
+    JobPostingEntity jobPosting = jobPostingService.createJobPosting(companyKey, request);
+
+    jobPostingTechStackService.createJobPostingTechStack(jobPosting, request);
+
+    jobPostingStepService.createJobPostingStep(jobPosting, request);
+
+    return ResponseEntity.ok("jobPostingKey: " + jobPosting.getJobPostingKey());
+  }
 }
