e3b0232b20f40e533150c57e8fdfc4e8a9538605
GLORY-JI <gigi716151@gmail.com>
2024-07-06 22:31:11 +0900

    feat: add creating job posting tech stack metod

--- src/main/java/com/ctrls/auto_enter_view/service/JobPostingTechStackService.java
+++ src/main/java/com/ctrls/auto_enter_view/service/JobPostingTechStackService.java
@@ -1,6 +1,11 @@
 package com.ctrls.auto_enter_view.service;
 
+import com.ctrls.auto_enter_view.dto.jobposting.JobPostingDto;
+import com.ctrls.auto_enter_view.entity.JobPostingEntity;
+import com.ctrls.auto_enter_view.entity.JobPostingTechStackEntity;
 import com.ctrls.auto_enter_view.repository.JobPostingTechStackRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,4 +16,17 @@ import org.springframework.stereotype.Service;
 public class JobPostingTechStackService {
 
   private final JobPostingTechStackRepository jobPostingTechStackRepository;
+
+  public void createJobPostingTechStack(JobPostingEntity jobPostingEntity,
+      JobPostingDto.Request request) {
+
+    List<String> techStack = request.getTechStack();
+
+    List<JobPostingTechStackEntity> entities = techStack.stream()
+        .map(e -> request.toTechStackEntity(jobPostingEntity, e))
+        .collect(Collectors.toList());
+
+    jobPostingTechStackRepository.saveAll(entities);
+
+  }
 }
