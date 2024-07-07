9d847b611b8243912bac5cd9cc779c6960ab526f
eunsun <eunsunjj9501@gmail.com>
2024-07-07 09:34:16 +0900

    refactor: Remove "Bearer" - 토큰을 블랙 리스트에 넣을 때 토큰 값만 추출하도록 substring으로 Bearer 삭제

--- src/main/java/com/ctrls/auto_enter_view/service/BlacklistTokenService.java
+++ src/main/java/com/ctrls/auto_enter_view/service/BlacklistTokenService.java
@@ -15,11 +15,16 @@ public class BlacklistTokenService {
 
   // 블랙 리스트에 토큰 추가
   public void addToBlacklist(String token) {
+    if (token.startsWith("Bearer ")) {
+      token = token.substring(7);
+    }
+    log.info("add blacklist token : {}", token);
     redisTemplate.opsForValue().set(token, "logout", 60, TimeUnit.MINUTES);
   }
 
   // 블랙 리스트에 토큰이 존재하는지 조회
   public boolean isTokenBlacklist(String token) {
+
     boolean isBlacklist = redisTemplate.opsForValue().get(token) != null;
     log.info(String.valueOf(isBlacklist));
 
