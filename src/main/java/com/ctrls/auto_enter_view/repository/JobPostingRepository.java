e4299acefc9f5643c0661db1282a511b41111a2e
GLORY-JI <gigi716151@gmail.com>
2024-07-08 22:20:50 +0900

    test

--- src/main/java/com/ctrls/auto_enter_view/repository/JobPostingRepository.java
+++ src/main/java/com/ctrls/auto_enter_view/repository/JobPostingRepository.java
@@ -8,11 +8,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JobPostingRepository extends JpaRepository<JobPostingEntity, String> {
-  
+
   Optional<JobPostingEntity> findByJobPostingKey(String jobPostingKey);
 
   List<JobPostingEntity> findAllByCompanyKey(String companyKey);
 
   void deleteByJobPostingKey(String jobPostingKey);
+  
 }
 
