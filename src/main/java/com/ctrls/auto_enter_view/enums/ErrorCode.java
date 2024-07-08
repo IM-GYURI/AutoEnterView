b2d10be2f25f591326dbb62b3265448640314e5d
Goldbar97 <ehrm1357@gmail.com>
2024-07-08 21:14:47 +0900

    feat: company info enums

--- src/main/java/com/ctrls/auto_enter_view/enums/ErrorCode.java
+++ src/main/java/com/ctrls/auto_enter_view/enums/ErrorCode.java
@@ -7,14 +7,15 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 
-  NOT_FOUND(404, "페이지를 찾을 수 없습니다."),
-  INTER_SERVER_ERROR(500, "내부 서버 오류"),
   EMAIL_DUPLICATION(400, "이메일이 중복됩니다."),
-  USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다."),
-  PASSWORD_NOT_MATCH(400, "비밀번호가 일치하지 않습니다"),
   EMAIL_NOT_FOUND(404, "가입된 사용자 이메일이 없습니다."),
   EMAIL_SEND_FAILURE(500, "이메일 전송에 실패했습니다."),
-  INVALID_VERIFICATION_CODE(400, "유효하지 않은 인증 코드입니다.");
+  INTER_SERVER_ERROR(500, "내부 서버 오류"),
+  INVALID_VERIFICATION_CODE(400, "유효하지 않은 인증 코드입니다."),
+  NOT_FOUND(404, "페이지를 찾을 수 없습니다."),
+  NO_AUTHORITY(401, "권한이 없습니다."),
+  PASSWORD_NOT_MATCH(400, "비밀번호가 일치하지 않습니다"),
+  USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다.");
 
   private final int status;
   private final String message;
--- src/main/java/com/ctrls/auto_enter_view/enums/ResponseMessage.java
+++ src/main/java/com/ctrls/auto_enter_view/enums/ResponseMessage.java
@@ -6,12 +6,15 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ResponseMessage {
-  SIGNUP("회원가입을 축하드립니다."),
-  USABLE_EMAIL("사용 가능한 이메일입니다."),
   CHANGE_PASSWORD("비밀번호 변경 완료."),
+  SIGNUP("회원가입을 축하드립니다."),
+  SUCCESS_CREATE_COMPANY_INFO("회사 정보 생성이 완료되었습니다."),
+  SUCCESS_DELETE_COMPANY_INFO("회사 정보 삭제가 완료되었습니다."),
+  SUCCESS_EMAIL_VERIFY("이메일 인증 성공."),
   SUCCESS_SEND_CODE("인증 코드 전송 성공."),
   SUCCESS_TEMPORARY_PASSWORD_SEND("임시 비밀번호 전송 성공."),
-  SUCCESS_EMAIL_VERIFY("이메일 인증 성공."),
+  SUCCESS_UPDATE_COMPANY_INFO("회사 정보 수정이 완료되었습니다."),
+  USABLE_EMAIL("사용 가능한 이메일입니다."),
   WITHDRAW("회원탈퇴 완료.");
 
   private final String message;
