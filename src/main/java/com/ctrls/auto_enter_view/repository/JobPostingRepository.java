d772221b56d182a6760d8792dd7c6f64d2ebdc84
GLORY-JI <gigi716151@gmail.com>
2024-07-08 18:50:27 +0900

    feat: add delete and find entity function

--- src/main/java/com/ctrls/auto_enter_view/repository/JobPostingRepository.java
+++ src/main/java/com/ctrls/auto_enter_view/repository/JobPostingRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JobPostingRepository extends JpaRepository<JobPostingEntity, String> {
 
+  JobPostingEntity findByJobPostingKey(String jobPostingKey);
+
+  void deleteByJobPostingKey(String jobPostingKey);
 }
--- src/main/java/com/ctrls/auto_enter_view/repository/JobPostingStepRepository.java
+++ src/main/java/com/ctrls/auto_enter_view/repository/JobPostingStepRepository.java
@@ -1,10 +1,14 @@
 package com.ctrls.auto_enter_view.repository;
 
 import com.ctrls.auto_enter_view.entity.JobPostingStepEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JobPostingStepRepository extends JpaRepository<JobPostingStepEntity, Long> {
 
+  List<JobPostingStepEntity> findByJobPostingKey(String jobPostingKey);
+
+  void deleteByJobPostingKey(String jobPostingKey);
 }
