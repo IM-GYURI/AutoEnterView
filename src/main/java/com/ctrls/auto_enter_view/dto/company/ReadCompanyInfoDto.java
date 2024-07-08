23d836a2da6e2d28f47f2aeb6d152184981907b1
Goldbar97 <ehrm1357@gmail.com>
2024-07-08 21:09:55 +0900

    feat: company info dto

---
+++ src/main/java/com/ctrls/auto_enter_view/dto/company/ReadCompanyInfoDto.java
@@ -0,0 +1,35 @@
+package com.ctrls.auto_enter_view.dto.company;
+
+import com.ctrls.auto_enter_view.entity.CompanyInfoEntity;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class ReadCompanyInfoDto {
+
+  @AllArgsConstructor
+  @Builder
+  @Getter
+  @NoArgsConstructor
+  public static class Response {
+
+    private int employees;
+    private LocalDate companyAge;
+    private String companyUrl;
+    private String boss;
+    private String address;
+
+    public static Response toDto(CompanyInfoEntity e) {
+
+      return Response.builder()
+          .employees(e.getEmployees())
+          .companyAge(e.getCompanyAge())
+          .companyUrl(e.getCompanyUrl())
+          .boss(e.getBoss())
+          .address(e.getBoss())
+          .build();
+    }
+  }
+}
