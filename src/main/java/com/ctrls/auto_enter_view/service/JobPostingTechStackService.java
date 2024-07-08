4e293f6edab5f60b06a06ce253442cfe1eca64b7
eunsun <eunsunjj9501@gmail.com>
2024-07-08 19:57:13 +0900

    feat: Add job-posting techStack search function - jobPostingKey 로 기술 스택을 조회 - List<String>으로 반환

--- src/main/java/com/ctrls/auto_enter_view/service/JobPostingTechStackService.java
+++ src/main/java/com/ctrls/auto_enter_view/service/JobPostingTechStackService.java
@@ -5,6 +5,7 @@ import com.ctrls.auto_enter_view.dto.jobposting.JobPostingDto.Request;
 import com.ctrls.auto_enter_view.entity.JobPostingEntity;
 import com.ctrls.auto_enter_view.entity.JobPostingTechStackEntity;
 import com.ctrls.auto_enter_view.repository.JobPostingTechStackRepository;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -29,5 +30,17 @@ public class JobPostingTechStackService {
 
     jobPostingTechStackRepository.saveAll(entities);
 
+  }
+
+  public List<String> getTechStackByJobPostingKey(String jobPostingKey) {
+    List<JobPostingTechStackEntity> entities = jobPostingTechStackRepository.findByJobPostingKey(jobPostingKey);
+    List<String> techStack = new ArrayList<>();
+
+    for (JobPostingTechStackEntity entity : entities) {
+      techStack.add(entity.getTechName());
+    }
+
+    log.info("techStack 가져오기 성공 {}", techStack);
+    return techStack;
   }
 }
