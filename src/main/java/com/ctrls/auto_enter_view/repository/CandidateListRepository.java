dd07c0706f001e3d9df6272040b2d78c26152e7c
eunsun <eunsunjj9501@gmail.com>
2024-07-10 02:10:08 +0900

    feat: Add method - findByCandidateKeyAndJobPostingKey : candidateKey로 지원자가 채용공고(jobPostingKey)에 지원한 정보를 찾음 - findAllByCandidateKey : 특정 지원자로 지원한 모든 채용 공고를 가져온다

--- src/main/java/com/ctrls/auto_enter_view/repository/CandidateListRepository.java
+++ src/main/java/com/ctrls/auto_enter_view/repository/CandidateListRepository.java
@@ -2,14 +2,19 @@ package com.ctrls.auto_enter_view.repository;
 
 import com.ctrls.auto_enter_view.entity.CandidateListEntity;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CandidateListRepository extends JpaRepository<CandidateListEntity, Long> {
+public interface CandidateListRepository extends JpaRepository<CandidateListEntity, String> {
 
   List<CandidateListEntity> findAllByJobPostingKeyAndJobPostingStepId(String jobPostingKey,
       Long jobPostingStepId);
 
   boolean existsByCandidateKeyAndJobPostingKey(String candidateKey, String jobPostingKey);
+
+  Optional<CandidateListEntity> findByCandidateKeyAndJobPostingKey(String candidateKey, String jobPostingKey);
+
+  List<CandidateListEntity> findAllByCandidateKey(String candidateKey);
 }
