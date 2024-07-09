1ccd515a50a20ba953156c86478f5fb3f6490045
eunsun <eunsunjj9501@gmail.com>
2024-07-10 02:37:57 +0900

    feat: Add candidate job-posting search function - 지원자가 본인이 지원한 채용 공고 조회 기능 추가

--- src/main/java/com/ctrls/auto_enter_view/service/CandidateService.java
+++ src/main/java/com/ctrls/auto_enter_view/service/CandidateService.java
@@ -5,15 +5,25 @@ import static com.ctrls.auto_enter_view.enums.ErrorCode.EMAIL_NOT_FOUND;
 import static com.ctrls.auto_enter_view.enums.ErrorCode.PASSWORD_NOT_MATCH;
 import static com.ctrls.auto_enter_view.enums.ErrorCode.USER_NOT_FOUND;
 
+import com.ctrls.auto_enter_view.dto.candidate.CandidateApplyDto;
 import com.ctrls.auto_enter_view.dto.candidate.ChangePasswordDto.Request;
 import com.ctrls.auto_enter_view.dto.candidate.FindEmailDto;
 import com.ctrls.auto_enter_view.dto.candidate.FindEmailDto.Response;
 import com.ctrls.auto_enter_view.dto.candidate.SignUpDto;
 import com.ctrls.auto_enter_view.dto.candidate.WithdrawDto;
 import com.ctrls.auto_enter_view.entity.CandidateEntity;
+import com.ctrls.auto_enter_view.entity.CandidateListEntity;
+import com.ctrls.auto_enter_view.entity.CompanyEntity;
+import com.ctrls.auto_enter_view.entity.JobPostingEntity;
 import com.ctrls.auto_enter_view.enums.ErrorCode;
 import com.ctrls.auto_enter_view.exception.CustomException;
+import com.ctrls.auto_enter_view.repository.CandidateListRepository;
 import com.ctrls.auto_enter_view.repository.CandidateRepository;
+import com.ctrls.auto_enter_view.repository.CompanyRepository;
+import com.ctrls.auto_enter_view.repository.JobPostingRepository;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -27,7 +37,9 @@ import org.springframework.stereotype.Service;
 public class CandidateService {
 
   private final CandidateRepository candidateRepository;
-
+  private final CompanyRepository companyRepository;
+  private final CandidateListRepository candidateListRepository;
+  private final JobPostingRepository jobPostingRepository;
   private final PasswordEncoder passwordEncoder;
 
   // 회원 가입
@@ -110,10 +122,43 @@ public class CandidateService {
         .orElseThrow(() -> new CustomException(EMAIL_NOT_FOUND));
   }
 
-  // candidateKey -> 지원자 정보 조회
+  // candidateKey -> 지원자 정보 조회 : 이름
   public String getCandidateNameByKey(String candidateKey) {
     return candidateRepository.findByCandidateKey(candidateKey)
         .map(CandidateEntity::getName)
         .orElseThrow(() -> new CustomException(ErrorCode.CANDIDATE_NOT_FOUND));
+  }
+
+  // 지원자가 지원한 채용 공고 조회
+  public CandidateApplyDto.Response getApplyJobPostings(String candidateKey) {
+    List<CandidateListEntity> candidateListEntities = candidateListRepository.findAllByCandidateKey(candidateKey);
+
+    List<CandidateApplyDto.ApplyInfo> applyInfoList = new ArrayList<>();
+
+    for (CandidateListEntity candidateListEntity : candidateListEntities) {
+      JobPostingEntity jobPostingEntity = jobPostingRepository.findByJobPostingKey(candidateListEntity.getJobPostingKey())
+          .orElseThrow(() -> new CustomException(ErrorCode.JOB_POSTING_NOT_FOUND));
+
+      String companyName = getCompanyName(jobPostingEntity.getCompanyKey());
+      LocalDateTime applyDate = candidateListEntity.getCreatedAt();
+
+      CandidateApplyDto.ApplyInfo applyInfo = CandidateApplyDto.ApplyInfo.from(jobPostingEntity, companyName, applyDate);
+      applyInfoList.add(applyInfo);
+    }
+
+    log.info("{}개의 지원한 채용 공고 조회 완료", applyInfoList.size());
+    return CandidateApplyDto.Response.builder()
+        .applyJobPostingsList(applyInfoList)
+        .build();
+  }
+
+  // 회사 이름 가져오기
+  private String getCompanyName(String companyKey) {
+    CompanyEntity companyEntity = companyRepository.findByCompanyKey(companyKey)
+        .orElseThrow(() -> new CustomException(ErrorCode.COMPANY_NOT_FOUND));
+
+    String companyName = companyEntity.getCompanyName();
+    log.info("회사명 조회 완료 : {}", companyName);
+    return companyName;
   }
 }
