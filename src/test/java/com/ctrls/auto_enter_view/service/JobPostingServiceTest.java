409b83b7d11a3543b71eb910907445f31478d0f9
GLORY-JI <gigi716151@gmail.com>
2024-07-09 22:11:11 +0900

    test: create job posting and edit job posting test code

--- src/test/java/com/ctrls/auto_enter_view/service/JobPostingServiceTest.java
+++ src/test/java/com/ctrls/auto_enter_view/service/JobPostingServiceTest.java
@@ -7,12 +7,15 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.ctrls.auto_enter_view.dto.jobPosting.JobPostingDto;
+import com.ctrls.auto_enter_view.dto.jobPosting.JobPostingDto.Request;
 import com.ctrls.auto_enter_view.dto.jobPosting.JobPostingInfoDto;
 import com.ctrls.auto_enter_view.entity.CompanyEntity;
 import com.ctrls.auto_enter_view.entity.JobPostingEntity;
 import com.ctrls.auto_enter_view.exception.CustomException;
 import com.ctrls.auto_enter_view.repository.CompanyRepository;
 import com.ctrls.auto_enter_view.repository.JobPostingRepository;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -20,6 +23,7 @@ import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -93,4 +97,122 @@ class JobPostingServiceTest {
     verify(companyRepository, times(1)).findByEmail(user.getUsername());
     assertEquals(USER_NOT_FOUND, exception.getErrorCode());
   }
+
+  @Test
+  @DisplayName("채용 공고 등록 성공 테스트")
+  void testCreateJobPosting() {
+    //given
+    String companyKey = "companyKey";
+
+    String jobPostingKey = "JobPostingKey";
+
+    JobPostingDto.Request request = Request.builder()
+        .title("title")
+        .jobCategory("jobCategory")
+        .career(3)
+        .workLocation("workLocation")
+        .education("education")
+        .employmentType("employmentType")
+        .salary(3000L)
+        .workTime("workTime")
+        .startDate(LocalDate.of(2024, 07, 15))
+        .endDate(LocalDate.of(2024, 07, 20))
+        .jobPostingContent("content")
+        .build();
+
+    JobPostingEntity jobPostingEntity = JobPostingEntity.builder()
+        .jobPostingKey(jobPostingKey)
+        .companyKey(companyKey)
+        .title("title")
+        .jobCategory("jobCategory")
+        .career(3)
+        .workLocation("workLocation")
+        .education("education")
+        .employmentType("employmentType")
+        .salary(3000L)
+        .workTime("workTime")
+        .startDate(LocalDate.of(2024, 07, 15))
+        .endDate(LocalDate.of(2024, 07, 20))
+        .jobPostingContent("content")
+        .build();
+
+    when(jobPostingRepository.save(jobPostingEntity)).thenReturn(jobPostingEntity);
+
+    ArgumentCaptor<JobPostingEntity> captor = ArgumentCaptor.forClass(JobPostingEntity.class);
+
+    //when
+    jobPostingService.createJobPosting(companyKey, request);
+
+    //then
+    verify(jobPostingRepository, times(1)).save(captor.capture());
+    JobPostingEntity captorValue = captor.getValue();
+
+    assertEquals(companyKey, captorValue.getCompanyKey());
+    assertEquals(request.getTitle(), captorValue.getTitle());
+    assertEquals(request.getJobCategory(), captorValue.getJobCategory());
+    assertEquals(request.getCareer(), captorValue.getCareer());
+    assertEquals(request.getWorkLocation(), captorValue.getWorkLocation());
+    assertEquals(request.getEducation(), captorValue.getEducation());
+    assertEquals(request.getEmploymentType(), captorValue.getEmploymentType());
+    assertEquals(request.getSalary(), captorValue.getSalary());
+    assertEquals(request.getWorkTime(), captorValue.getWorkTime());
+    assertEquals(request.getStartDate(), captorValue.getStartDate());
+    assertEquals(request.getEndDate(), captorValue.getEndDate());
+    assertEquals(request.getJobPostingContent(), captorValue.getJobPostingContent());
+
+  }
+
+  @Test
+  @DisplayName("채용 공고 수정 성공 테스트")
+  void testEditJobPosting() {
+    //given
+    String companyKey = "companyKey";
+
+    String jobPostingKey = "JobPostingKey";
+
+    JobPostingDto.Request request = Request.builder()
+        .title("edit title")
+        .jobCategory("jobCategory")
+        .career(3)
+        .workLocation("workLocation")
+        .education("education")
+        .employmentType("edit employmentType")
+        .salary(3000L)
+        .workTime("workTime")
+        .startDate(LocalDate.of(2024, 07, 15))
+        .endDate(LocalDate.of(2024, 07, 20))
+        .jobPostingContent("content")
+        .build();
+
+    JobPostingEntity jobPostingEntity = JobPostingEntity.builder()
+        .jobPostingKey(jobPostingKey)
+        .companyKey(companyKey)
+        .title("title")
+        .jobCategory("jobCategory")
+        .career(3)
+        .workLocation("workLocation")
+        .education("education")
+        .employmentType("employmentType")
+        .salary(3000L)
+        .workTime("workTime")
+        .startDate(LocalDate.of(2024, 07, 15))
+        .endDate(LocalDate.of(2024, 07, 20))
+        .jobPostingContent("content")
+        .build();
+
+    when(jobPostingRepository.findByJobPostingKey(jobPostingKey)).thenReturn(
+        Optional.of(jobPostingEntity));
+
+    //when
+    jobPostingService.editJobPosting(jobPostingKey, request);
+
+    //then
+    verify(jobPostingRepository, times(1));
+
+    //제목, 고용타입만 바뀌는것 확인
+    assertEquals(request.getTitle(), jobPostingEntity.getTitle());
+    assertEquals(request.getEmploymentType(), jobPostingEntity.getEmploymentType());
+
+  }
+
 }
