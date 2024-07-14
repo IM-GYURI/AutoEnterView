e8c3d506c0e007cc032e5fecac092ac6b913b359
GLORY-JI <gigi716151@gmail.com>
2024-07-14 17:25:01 +0900

    feat: add post method on inverview schedule controller

--- src/main/java/com/ctrls/auto_enter_view/controller/InterviewScheduleController.java
+++ src/main/java/com/ctrls/auto_enter_view/controller/InterviewScheduleController.java
@@ -2,6 +2,9 @@ package com.ctrls.auto_enter_view.controller;
 
 import com.ctrls.auto_enter_view.service.InterviewScheduleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -9,4 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class InterviewScheduleController {
 
   private final InterviewScheduleService interviewScheduleService;
+
+
+  @PostMapping("/job-postings/{jobPostingKey}/steps/{stepId}/interview-schedule")
+  public ResponseEntity<String> createInterviewSchedule(@PathVariable String jobPostingKey,
+      @PathVariable Long stepId) {
+
+    interviewScheduleService.createInterviewSchedule(jobPostingKey, stepId);
+
+    return ResponseEntity.ok("면접 일정 생성 완료.");
+  }
 }
