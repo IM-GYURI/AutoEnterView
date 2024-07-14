682672fc03a88b74396bd3f8437deded86ed5d97
GLORY-JI <gigi716151@gmail.com>
2024-07-14 21:21:59 +0900

    refacotr: add dto

--- src/main/java/com/ctrls/auto_enter_view/controller/InterviewScheduleController.java
+++ src/main/java/com/ctrls/auto_enter_view/controller/InterviewScheduleController.java
@@ -1,10 +1,14 @@
 package com.ctrls.auto_enter_view.controller;
 
+import com.ctrls.auto_enter_view.dto.interviewschedule.InterviewScheduleDto;
+import com.ctrls.auto_enter_view.dto.interviewschedule.InterviewScheduleDto.Request;
 import com.ctrls.auto_enter_view.service.InterviewScheduleService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -16,9 +20,9 @@ public class InterviewScheduleController {
 
   @PostMapping("/job-postings/{jobPostingKey}/steps/{stepId}/interview-schedule")
   public ResponseEntity<String> createInterviewSchedule(@PathVariable String jobPostingKey,
-      @PathVariable Long stepId) {
+      @PathVariable Long stepId, @RequestBody List<Request> request) {
 
-    interviewScheduleService.createInterviewSchedule(jobPostingKey, stepId);
+    interviewScheduleService.createInterviewSchedule(jobPostingKey, stepId, request);
 
     return ResponseEntity.ok("면접 일정 생성 완료.");
   }
