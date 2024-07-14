53a33900bd916ec399bce97d75bc4cfa33f1bd9e
GLORY-JI <gigi716151@gmail.com>
2024-07-14 21:22:19 +0900

    feat: add toentity method

--- src/main/java/com/ctrls/auto_enter_view/dto/interviewschedule/InterviewScheduleDto.java
+++ src/main/java/com/ctrls/auto_enter_view/dto/interviewschedule/InterviewScheduleDto.java
@@ -1,11 +1,15 @@
 package com.ctrls.auto_enter_view.dto.interviewschedule;
 
+import com.ctrls.auto_enter_view.entity.InterviewScheduleEntity;
 import java.sql.Time;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
 import org.springframework.format.annotation.DateTimeFormat;
 
 public class InterviewScheduleDto {
@@ -16,15 +20,29 @@ public class InterviewScheduleDto {
   @NoArgsConstructor
   public static class Request {
 
-    LocalDate startDate;
+    private LocalDate startDate;
 
     @DateTimeFormat(pattern = "HH:mm")
-    Time startTime;
+    private LocalTime startTime;
 
     @DateTimeFormat(pattern = "mm")
-    Time term;
+    private LocalTime term;
 
-    Long times;
+    private Integer times;
+
+    public static InterviewScheduleEntity toEntity(String jobPostingKey, Long stepId,
+        List<Request> request) {
+
+      LocalDate firstDate = request.get(0).getStartDate();
+      LocalDate lastDate = request.get(request.size() - 1).getStartDate();
+
+      return InterviewScheduleEntity.builder()
+          .jobPostingStepId(stepId)
+          .jobPostingKey(jobPostingKey)
+          .firstInterviewDate(firstDate)
+          .lastInterviewDate(lastDate)
+          .build();
+    }
 
   }
 
