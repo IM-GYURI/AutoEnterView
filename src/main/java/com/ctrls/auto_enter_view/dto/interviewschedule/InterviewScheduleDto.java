b7bf050bc4aa4d0bd73e558ec0bb68b0f110fd12
GLORY-JI <gigi716151@gmail.com>
2024-07-15 17:24:55 +0900

    feat: create interview schedule participants entity method

--- src/main/java/com/ctrls/auto_enter_view/dto/interviewschedule/InterviewScheduleDto.java
+++ src/main/java/com/ctrls/auto_enter_view/dto/interviewschedule/InterviewScheduleDto.java
@@ -1,9 +1,11 @@
 package com.ctrls.auto_enter_view.dto.interviewschedule;
 
 import com.ctrls.auto_enter_view.entity.InterviewScheduleEntity;
+import com.ctrls.auto_enter_view.entity.InterviewScheduleParticipantsEntity;
 import com.ctrls.auto_enter_view.util.KeyGenerator;
 import java.sql.Time;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -14,6 +16,9 @@ import org.springframework.cglib.core.Local;
 import org.springframework.format.annotation.DateTimeFormat;
 
 public class InterviewScheduleDto {
+
+  public static String interviewScheduleKey = KeyGenerator.generateKey();
+
 
   @Getter
   @Builder
@@ -37,11 +42,24 @@ public class InterviewScheduleDto {
       LocalDate lastDate = request.get(request.size() - 1).getStartDate();
 
       return InterviewScheduleEntity.builder()
-          .interviewScheduleKey(KeyGenerator.generateKey())
+          .interviewScheduleKey(interviewScheduleKey)
           .jobPostingStepId(stepId)
           .jobPostingKey(jobPostingKey)
           .firstInterviewDate(firstDate)
           .lastInterviewDate(lastDate)
+          .build();
+    }
+
+    public static InterviewScheduleParticipantsEntity toParticipantsEntity(String jobPostingKey,
+        Long stepId, LocalDateTime startDateTime, LocalDateTime endDateTime, String candidateKey) {
+
+      return InterviewScheduleParticipantsEntity.builder()
+          .interviewScheduleKey(interviewScheduleKey)
+          .candidateKey(candidateKey)
+          .jobPostingKey(jobPostingKey)
+          .jobPostingStepId(stepId)
+          .interviewStartDatetime(startDateTime)
+          .interviewEndDatetime(endDateTime)
           .build();
     }
 
