ae39f2fcbd67165b3adb2ca6d9cd4695f7c528a2
Goldbar97 <ehrm1357@gmail.com>
2024-07-15 22:53:07 +0900

    feat: tech stack enum

--- src/main/java/com/ctrls/auto_enter_view/repository/ResumeTechStackRepository.java
+++ src/main/java/com/ctrls/auto_enter_view/repository/ResumeTechStackRepository.java
@@ -3,10 +3,16 @@ package com.ctrls.auto_enter_view.repository;
 import com.ctrls.auto_enter_view.entity.ResumeTechStackEntity;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ResumeTechStackRepository extends JpaRepository<ResumeTechStackEntity, Long> {
 
   List<ResumeTechStackEntity> findAllByResumeKey(String resumeKey);
+
+  @Modifying
+  @Query("DELETE FROM ResumeTechStackEntity r WHERE r.resumeKey = :resumeKey")
+  void deleteAllByResumeKey(String resumeKey);
 }
