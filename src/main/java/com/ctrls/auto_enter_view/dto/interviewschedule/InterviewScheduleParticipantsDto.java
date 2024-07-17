179ca59ca3dd25de2ec2e145429b0b5bf49a756e
GLORY-JI <gigi716151@gmail.com>
2024-07-17 14:54:51 +0900

    feat: add participants DTO

---
+++ src/main/java/com/ctrls/auto_enter_view/dto/interviewschedule/InterviewScheduleParticipantsDto.java
@@ -0,0 +1,40 @@
+package com.ctrls.auto_enter_view.dto.interviewschedule;
+
+import com.ctrls.auto_enter_view.entity.InterviewScheduleParticipantsEntity;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class InterviewScheduleParticipantsDto {
+
+
+  @Getter
+  @Builder
+  @AllArgsConstructor
+  @NoArgsConstructor
+  public static class Response {
+
+    private String interviewScheduleKey;
+
+    private String candidateKey;
+
+    private LocalDateTime interviewStartDateTime;
+
+    private LocalDateTime interviewEndDateTime;
+
+
+    public static Response fromEntity(InterviewScheduleParticipantsEntity entity) {
+
+      return Response.builder()
+          .interviewScheduleKey(entity.getInterviewScheduleKey())
+          .candidateKey(entity.getCandidateKey())
+          .interviewStartDateTime(entity.getInterviewStartDatetime())
+          .interviewEndDateTime(entity.getInterviewEndDatetime())
+          .build();
+    }
+
+  }
+
+}
