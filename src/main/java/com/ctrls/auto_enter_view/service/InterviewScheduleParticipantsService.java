9225aece992e5533b76bc1dcff44618470c37fef
eunsun <eunsunjj9501@gmail.com>
2024-07-17 22:20:34 +0900

    refactor: import 삭제

--- src/main/java/com/ctrls/auto_enter_view/service/InterviewScheduleParticipantsService.java
+++ src/main/java/com/ctrls/auto_enter_view/service/InterviewScheduleParticipantsService.java
@@ -10,7 +10,6 @@ import com.ctrls.auto_enter_view.repository.CandidateListRepository;
 import com.ctrls.auto_enter_view.repository.InterviewScheduleParticipantsRepository;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
