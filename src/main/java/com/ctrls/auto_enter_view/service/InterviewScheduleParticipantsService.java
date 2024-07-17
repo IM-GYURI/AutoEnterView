5524ba6040b7f287d1d14b95593eb714c53080b0
GLORY-JI <gigi716151@gmail.com>
2024-07-17 22:46:45 +0900

    feat: add update logic

--- src/main/java/com/ctrls/auto_enter_view/service/InterviewScheduleParticipantsService.java
+++ src/main/java/com/ctrls/auto_enter_view/service/InterviewScheduleParticipantsService.java
@@ -2,18 +2,23 @@ package com.ctrls.auto_enter_view.service;
 
 import com.ctrls.auto_enter_view.dto.candidateList.CandidateListDto;
 import com.ctrls.auto_enter_view.dto.interviewschedule.InterviewScheduleDto.Request;
+import com.ctrls.auto_enter_view.dto.interviewschedule.InterviewScheduleParticipantsDto;
 import com.ctrls.auto_enter_view.dto.interviewschedule.InterviewScheduleParticipantsDto.Response;
+import com.ctrls.auto_enter_view.entity.CandidateListEntity;
+import com.ctrls.auto_enter_view.entity.InterviewScheduleEntity;
 import com.ctrls.auto_enter_view.entity.InterviewScheduleParticipantsEntity;
 import com.ctrls.auto_enter_view.enums.ErrorCode;
 import com.ctrls.auto_enter_view.exception.CustomException;
 import com.ctrls.auto_enter_view.repository.CandidateListRepository;
 import com.ctrls.auto_enter_view.repository.InterviewScheduleParticipantsRepository;
+import com.ctrls.auto_enter_view.repository.InterviewScheduleRepository;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -23,6 +28,8 @@ public class InterviewScheduleParticipantsService {
   private final InterviewScheduleParticipantsRepository interviewScheduleParticipantsRepository;
 
   private final CandidateListRepository candidateListRepository;
+
+  private final InterviewScheduleRepository interviewScheduleRepository;
 
   public void createInterviewSchedule(String jobPostingKey, Long stepId, List<Request> request) {
 
@@ -68,5 +75,58 @@ public class InterviewScheduleParticipantsService {
         stepId);
 
     return entities.stream().map(Response::fromEntity).toList();
+  }
+
+  // 개인 면접 일정 수정
+  @Transactional
+  public void updatePersonalInterviewSchedule(String interviewScheduleKey, String candidateKey,
+      InterviewScheduleParticipantsDto.Request request) {
+
+    InterviewScheduleEntity interviewScheduleEntity = interviewScheduleRepository.findByInterviewScheduleKey(
+            interviewScheduleKey)
+        .orElseThrow(() -> new CustomException(ErrorCode.INTERVIEW_SCHEDULE_NOT_FOUND));
+
+    InterviewScheduleParticipantsEntity participantsEntity = interviewScheduleParticipantsRepository.findByInterviewScheduleKeyAndCandidateKey(
+        interviewScheduleKey, candidateKey);
+
+    // 변경될 사항이 첫번째 인터뷰 날짜인지, 마지막 인터뷰 날짜인지 체크
+    if (isFirstInterview(participantsEntity, interviewScheduleEntity)) {
+
+      interviewScheduleEntity.updateFirstInterviewDate(
+          request.getInterviewStartDatetime().toLocalDate());
+    } else if (isLastInterview(participantsEntity, interviewScheduleEntity)) {
+
+      interviewScheduleEntity.updateLastInterviewDate(request.getInterviewEndDatetime()
+          .toLocalDate());
+    }
+
+    participantsEntity.updateEntity(request);
+
+  }
+
+  // 개인 면접 일정 삭제
+  @Transactional
+  public void deletePersonalInterviewSchedule(String interviewScheduleKey, String candidateKey) {
+
+    InterviewScheduleParticipantsEntity entity = interviewScheduleParticipantsRepository.findByInterviewScheduleKeyAndCandidateKey(
+        interviewScheduleKey, candidateKey);
+
+    interviewScheduleParticipantsRepository.delete(entity);
+  }
+
+  // 첫번째 인터뷰 날짜인지 확인
+  public boolean isFirstInterview(InterviewScheduleParticipantsEntity participantsEntity,
+      InterviewScheduleEntity interviewScheduleEntity) {
+
+    return participantsEntity.getInterviewStartDatetime().toLocalDate()
+        .isEqual(interviewScheduleEntity.getFirstInterviewDate());
+  }
+
+  // 마지막 인터뷰 날짜인지 확인
+  public boolean isLastInterview(InterviewScheduleParticipantsEntity participantsEntity,
+      InterviewScheduleEntity interviewScheduleEntity) {
+
+    return participantsEntity.getInterviewEndDatetime().toLocalDate()
+        .isEqual(interviewScheduleEntity.getLastInterviewDate());
   }
 }
