c54442db25c16f10d50f493217c19ebe458b9651
eunsun <eunsunjj9501@gmail.com>
2024-07-19 01:04:28 +0900

    feat: 지원자 탈퇴 시 채용 공고에 지원한 지원자 삭제 - @Modify 어노테이션으로 candidateKey와 일치하면 삭제 - 일치하는 데이터가 없어도 에러 발생하지 않음

--- src/main/java/com/ctrls/auto_enter_view/repository/ApplicantRepository.java
+++ src/main/java/com/ctrls/auto_enter_view/repository/ApplicantRepository.java
@@ -3,6 +3,8 @@ package com.ctrls.auto_enter_view.repository;
 import com.ctrls.auto_enter_view.entity.ApplicantEntity;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,4 +13,8 @@ public interface ApplicantRepository extends JpaRepository<ApplicantEntity, Long
   List<ApplicantEntity> findAllByJobPostingKey(String jobPostingKey);
 
   boolean existsByCandidateKeyAndJobPostingKey(String candidateKey, String jobPostingKey);
+
+  @Modifying
+  @Query("DELETE FROM ApplicantEntity r WHERE r.candidateKey = :candidateKey")
+  void deleteByCandidateKey(String candidateKey);
 }
