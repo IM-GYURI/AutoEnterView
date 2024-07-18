a7c1903de4a3b56603a5cf9e43a58906f4d91152
IM-GYURI <kgulr0517@gmail.com>
2024-07-19 03:12:48 +0900

    feat : add JobPostingEveryInfoDto
    - 채용 단계 id와 해당 채용 단계에 존재하는 지원자 목록을 보여주기 위한 Dto

---
+++ src/main/java/com/ctrls/auto_enter_view/dto/jobPosting/JobPostingEveryInfoDto.java
@@ -0,0 +1,19 @@
+package com.ctrls.auto_enter_view.dto.jobPosting;
+
+import com.ctrls.auto_enter_view.dto.candidateList.CandidateTechStackInterviewInfoDto;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JobPostingEveryInfoDto {
+
+  private Long stepId;
+
+  private List<CandidateTechStackInterviewInfoDto> candidateTechStackInterviewInfoDtoList;
+}
