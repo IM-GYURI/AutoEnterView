c34070ad70fe081147405934cfd71c65b5478263
IM-GYURI <kgulr0517@gmail.com>
2024-07-19 21:17:22 +0900

    feat : divide into 2 condition
    2가지 조건으로 분리
    - 과제일 경우 실행될 로직
    - 면접일 경우 실행될 로직

--- src/main/java/com/ctrls/auto_enter_view/component/MailJob.java
+++ src/main/java/com/ctrls/auto_enter_view/component/MailJob.java
@@ -1,11 +1,16 @@
 package com.ctrls.auto_enter_view.component;
 
+import com.ctrls.auto_enter_view.entity.CandidateListEntity;
+import com.ctrls.auto_enter_view.entity.InterviewScheduleEntity;
 import com.ctrls.auto_enter_view.entity.InterviewScheduleParticipantsEntity;
 import com.ctrls.auto_enter_view.entity.MailAlarmInfoEntity;
+import com.ctrls.auto_enter_view.repository.CandidateListRepository;
 import com.ctrls.auto_enter_view.repository.InterviewScheduleParticipantsRepository;
+import com.ctrls.auto_enter_view.repository.InterviewScheduleRepository;
 import com.ctrls.auto_enter_view.repository.MailAlarmInfoRepository;
 import com.ctrls.auto_enter_view.service.MailAlarmInfoService;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -18,6 +23,8 @@ public class MailJob implements Job {
 
   private final MailAlarmInfoService mailAlarmInfoService;
   private final MailAlarmInfoRepository mailAlarmInfoRepository;
+  private final InterviewScheduleRepository interviewScheduleRepository;
+  private final CandidateListRepository candidateListRepository;
   private final InterviewScheduleParticipantsRepository interviewScheduleParticipantsRepository;
 
   @Override
@@ -26,9 +33,29 @@ public class MailJob implements Job {
     MailAlarmInfoEntity mailAlarmInfoEntity = mailAlarmInfoRepository.findById(mailId)
         .orElseThrow(() -> new JobExecutionException("Mail not found"));
 
-    List<InterviewScheduleParticipantsEntity> participants = interviewScheduleParticipantsRepository.findAllByInterviewScheduleKey(
-        mailAlarmInfoEntity.getInterviewScheduleKey());
+    String interviewScheduleKey = mailAlarmInfoEntity.getInterviewScheduleKey();
+    Optional<InterviewScheduleEntity> interviewScheduleEntity = interviewScheduleRepository.findByInterviewScheduleKey(
+        interviewScheduleKey);
 
-    mailAlarmInfoService.sendMailToCandidates(participants, mailAlarmInfoEntity);
+    boolean isTask = interviewScheduleEntity.isPresent()
+        && interviewScheduleEntity.get().getFirstInterviewDate() == null;
+    boolean isInterview = interviewScheduleEntity.isPresent()
+        && interviewScheduleEntity.get().getFirstInterviewDate() != null;
+
+    if (isTask) {
+      // 과제일 경우
+      List<CandidateListEntity> participants = candidateListRepository.findAllByJobPostingKeyAndJobPostingStepId(
+          mailAlarmInfoEntity.getJobPostingKey(), mailAlarmInfoEntity.getJobPostingStepId());
+
+      mailAlarmInfoService.sendTaskMailToCandidates(participants, mailAlarmInfoEntity);
+    }
+
+    if (isInterview) {
+      // 면접일 경우
+      List<InterviewScheduleParticipantsEntity> participants = interviewScheduleParticipantsRepository.findAllByInterviewScheduleKey(
+          mailAlarmInfoEntity.getInterviewScheduleKey());
+
+      mailAlarmInfoService.sendInterviewMailToCandidates(participants, mailAlarmInfoEntity);
+    }
   }
 }
