e254b79604a865f2d6d68fa4b96c984248eef941
eunsun <eunsunjj9501@gmail.com>
2024-07-20 13:50:20 +0900

    feat: JobCategory enum 생성 - 직무 일치를 비교하기 위해서 enum class 생성

---
+++ src/main/java/com/ctrls/auto_enter_view/enums/JobCategory.java
@@ -0,0 +1,16 @@
+package com.ctrls.auto_enter_view.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum JobCategory {
+  BACKEND("서버/백엔드 개발"),
+  FRONTEND("프론트엔드 개발"),
+  FULL("웹 풀스택 개발"),
+  ANDROID("안드로이드 개발"),
+  IOS("iOS 개발");
+
+  private final String value;
+}
