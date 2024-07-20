27685239e643e1606e976dc10b87e34d4437b600
IM-GYURI <kgulr0517@gmail.com>
2024-07-20 15:43:24 +0900

    feat :add ScoringJob
    execute() 구현
    - jobPosting의 applicant 리스트를 받아옴
    - applicant마다 지원자별로 이력서 점수를 매김

---
+++ src/main/java/com/ctrls/auto_enter_view/component/ScoringJob.java
@@ -0,0 +1,31 @@
+package com.ctrls.auto_enter_view.component;
+
+import com.ctrls.auto_enter_view.entity.ApplicantEntity;
+import com.ctrls.auto_enter_view.repository.ApplicantRepository;
+import com.ctrls.auto_enter_view.service.FilteringService;
+import java.util.List;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScoringJob implements Job {
+
+  private FilteringService filteringService;
+  private ApplicantRepository applicantRepository;
+
+  @Override
+  public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+    String jobPostingKey = jobExecutionContext.getJobDetail().getJobDataMap()
+        .getString("jobPostingKey");
+
+    // 해당 jobPosting의 applicantEntity 리스트를 받아오기
+    List<ApplicantEntity> applicants = applicantRepository.findAllByJobPostingKey(jobPostingKey);
+
+    for (ApplicantEntity applicantEntity : applicants) {
+      // 지원자별로 이력서 점수 매기기
+      filteringService.calculateResumeScore(applicantEntity.getCandidateKey(), jobPostingKey);
+    }
+  }
+}
