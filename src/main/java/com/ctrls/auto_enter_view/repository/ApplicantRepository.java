fbda11b24b299ddd0ba993995c35016bb80163f9
eunsun <eunsunjj9501@gmail.com>
2024-07-20 14:07:37 +0900

    feat: candidateKey, jobPostingKey로 ApplicantEntity 조회

--- src/main/java/com/ctrls/auto_enter_view/repository/ApplicantRepository.java
+++ src/main/java/com/ctrls/auto_enter_view/repository/ApplicantRepository.java
@@ -2,6 +2,7 @@ package com.ctrls.auto_enter_view.repository;
 
 import com.ctrls.auto_enter_view.entity.ApplicantEntity;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -17,4 +18,6 @@ public interface ApplicantRepository extends JpaRepository<ApplicantEntity, Long
   @Modifying
   @Query("DELETE FROM ApplicantEntity r WHERE r.candidateKey = :candidateKey")
   void deleteByCandidateKey(String candidateKey);
+
+  Optional<ApplicantEntity> findByCandidateKeyAndJobPostingKey(String candidateKey, String jobPostingKey);
 }
