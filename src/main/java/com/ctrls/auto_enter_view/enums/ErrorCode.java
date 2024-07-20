6b56c8705fe41bd30ba716446bcd582588d40d43
eunsun <eunsunjj9501@gmail.com>
2024-07-20 14:09:16 +0900

    feat: 에러 코드 추가

--- src/main/java/com/ctrls/auto_enter_view/enums/ErrorCode.java
+++ src/main/java/com/ctrls/auto_enter_view/enums/ErrorCode.java
@@ -22,6 +22,7 @@ public enum ErrorCode {
   NOT_FOUND(404, "페이지를 찾을 수 없습니다."),
   NO_AUTHORITY(401, "권한이 없습니다."),
   PASSWORD_NOT_MATCH(400, "비밀번호가 일치하지 않습니다."),
+  APPLICANT_NOT_FOUND(404, "지원자를 찾을 수 없습니다."),
   RESUME_NOT_FOUND(404, "지원자의 이력서를 찾을 수 없습니다"),
   TOKEN_BLACKLISTED(401, "토큰이 블랙리스트에 존재하여 사용할 수 없는 토큰입니다."),
   USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다."),
