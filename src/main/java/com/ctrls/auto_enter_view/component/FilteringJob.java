53afe7f1fd45b58f0c170249bed020110173eca1
IM-GYURI <kgulr0517@gmail.com>
2024-07-20 17:31:38 +0900

    fix : add @RequiredArgsConstructor
    - 의존성 주입 에러 발생

--- src/main/java/com/ctrls/auto_enter_view/component/FilteringJob.java
+++ src/main/java/com/ctrls/auto_enter_view/component/FilteringJob.java
@@ -1,15 +1,17 @@
 package com.ctrls.auto_enter_view.component;
 
 import com.ctrls.auto_enter_view.service.FilteringService;
+import lombok.RequiredArgsConstructor;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class FilteringJob implements Job {
 
-  private FilteringService filteringService;
+  private final FilteringService filteringService;
 
   @Override
   public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
