ac5e13294a822c2d0b289d808ba41957332f64e0
IM-GYURI <kgulr0517@gmail.com>
2024-07-22 14:07:15 +0900

    refactor : change Service and Method
    from FilteringService.calculateResumeScore() to ScoringService.scoreApplicants

--- src/main/java/com/ctrls/auto_enter_view/component/ScoringJob.java
+++ src/main/java/com/ctrls/auto_enter_view/component/ScoringJob.java
@@ -1,9 +1,6 @@
 package com.ctrls.auto_enter_view.component;
 
-import com.ctrls.auto_enter_view.entity.ApplicantEntity;
-import com.ctrls.auto_enter_view.repository.ApplicantRepository;
-import com.ctrls.auto_enter_view.service.FilteringService;
-import java.util.List;
+import com.ctrls.auto_enter_view.service.ScoringService;
 import lombok.RequiredArgsConstructor;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -14,20 +11,13 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ScoringJob implements Job {
 
-  private final FilteringService filteringService;
-  private final ApplicantRepository applicantRepository;
+  private final ScoringService scoringService;
 
   @Override
   public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
     String jobPostingKey = jobExecutionContext.getJobDetail().getJobDataMap()
         .getString("jobPostingKey");
 
-    // 해당 jobPosting의 applicantEntity 리스트를 받아오기
-    List<ApplicantEntity> applicants = applicantRepository.findAllByJobPostingKey(jobPostingKey);
-
-    for (ApplicantEntity applicantEntity : applicants) {
-      // 지원자별로 이력서 점수 매기기
-      filteringService.calculateResumeScore(applicantEntity.getCandidateKey(), jobPostingKey);
-    }
+    scoringService.scoreApplicants(jobPostingKey);
   }
 }
