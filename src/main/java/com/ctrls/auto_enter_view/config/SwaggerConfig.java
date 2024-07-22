eb082db0027725682451b0b845b8d45bf353c1ed
Goldbar97 <ehrm1357@gmail.com>
2024-07-23 00:35:52 +0900

    refactor: config 파일 설정 수정

--- src/main/java/com/ctrls/auto_enter_view/config/SwaggerConfig.java
+++ src/main/java/com/ctrls/auto_enter_view/config/SwaggerConfig.java
@@ -4,15 +4,13 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class SwaggerConfig implements WebMvcConfigurer {
+public class SwaggerConfig {
 
   @Bean
   public OpenAPI openAPI() {
+
     return new OpenAPI()
         .info(new Info()
             .title("AutoEnterView")
