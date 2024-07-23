8b60c0c03369ad4fe606a2ca3f05e37045969fbe
eunsun <eunsunjj9501@gmail.com>
2024-07-23 21:50:53 +0900

    refactor: 주석 추가 + 이름 변경

--- src/main/java/com/ctrls/auto_enter_view/controller/InterviewScheduleController.java
+++ src/main/java/com/ctrls/auto_enter_view/controller/InterviewScheduleController.java
@@ -20,7 +20,15 @@ public class InterviewScheduleController {
 
   private final InterviewScheduleService interviewScheduleService;
 
-  // 면접 일정 생성
+  /**
+   * 면접 일정 생성하기
+   *
+   * @param jobPostingKey 채용 공고 PK
+   * @param stepId 채용 단계 PK
+   * @param request List<InterviewScheduleDto.Request>
+   * @param userDetails 로그인 된 사용자 정보
+   * @return InterviewScheduleDto.Response
+   */
   @PostMapping("/job-postings/{jobPostingKey}/steps/{stepId}/interview-schedule")
   public ResponseEntity<Response> createInterviewSchedule(@PathVariable String jobPostingKey,
       @PathVariable Long stepId, @RequestBody List<Request> request,
@@ -32,15 +40,23 @@ public class InterviewScheduleController {
     return ResponseEntity.ok().body(response);
   }
 
-  // 과제 일정 생성
+  /**
+   * 과제 일정 생성 하기
+   *
+   * @param jobPostingKey 채용 공고 PK
+   * @param stepId 채용 단계 PK
+   * @param request InterviewScheduleDto.TaskRequest
+   * @param userDetails 로그인 된 사용자 정보
+   * @return InterviewScheduleDto.Response
+   */
   @PostMapping("/job-postings/{jobPostingKey}/steps/{stepId}/task-schedule")
   public ResponseEntity<Response> createTaskSchedule(@PathVariable String jobPostingKey,
-      @PathVariable Long stepId, @RequestBody TaskRequest taskRequest,
+      @PathVariable Long stepId, @RequestBody TaskRequest request,
       @AuthenticationPrincipal UserDetails userDetails) {
 
     Response response = interviewScheduleService.createTaskSchedule(jobPostingKey, stepId,
-        taskRequest, userDetails);
+        request, userDetails);
 
-    return ResponseEntity.ok().body(response);
+    return ResponseEntity.ok(response);
   }
 }
