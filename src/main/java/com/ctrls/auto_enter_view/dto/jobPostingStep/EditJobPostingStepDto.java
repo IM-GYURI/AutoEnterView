31da86468405debced09966ac187c997bea51328
eunsun <eunsunjj9501@gmail.com>
2024-07-24 16:26:52 +0900

    refactor: 여러명의 지원자를 한번에 단계이동 시킬 수 있도록 List<String>으로 candidateKey type 수정

--- src/main/java/com/ctrls/auto_enter_view/dto/jobPostingStep/EditJobPostingStepDto.java
+++ src/main/java/com/ctrls/auto_enter_view/dto/jobPostingStep/EditJobPostingStepDto.java
@@ -1,6 +1,7 @@
 package com.ctrls.auto_enter_view.dto.jobPostingStep;
 
 import jakarta.validation.constraints.NotBlank;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,5 +16,5 @@ public class EditJobPostingStepDto {
   private long currentStepId;
 
   @NotBlank(message = "지원자 키는 필수 입력값 입니다.")
-  private String candidateKey;
+  private List<String> candidateKey;
 }
