2e1363df6421934b9238af5c5c8a7e49566e0b17
IM-GYURI <kgulr0517@gmail.com>
2024-08-13 17:52:31 +0900

    feat : add OAuth2Response

---
+++ src/main/java/com/ctrls/auto_enter_view/dto/candidate/OAuth2Response.java
@@ -0,0 +1,14 @@
+package com.ctrls.auto_enter_view.dto.candidate;
+
+public interface OAuth2Response {
+
+  String getProvider();
+
+  String getProviderId();
+
+  String getEmail();
+
+  String getName();
+
+  String getNumber();
+}
