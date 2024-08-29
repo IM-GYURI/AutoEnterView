cdc174e9e833c8776efb7269ebecbb15d6ef8c91
IM-GYURI <kgulr0517@gmail.com>
2024-08-29 16:18:11 +0900

    feat : add "/job-postings/search" for search by keyword

--- src/main/java/com/ctrls/auto_enter_view/controller/SearchController.java
+++ src/main/java/com/ctrls/auto_enter_view/controller/SearchController.java
@@ -7,26 +7,34 @@ import com.ctrls.auto_enter_view.service.SearchService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/job-postings")
 public class SearchController {
 
   private final SearchService searchService;
 
-
-  @GetMapping("/api/search")
+  @GetMapping("/filter")
   public List<String> searchJobPostings(
       @RequestParam(required = false) JobCategory position,
       @RequestParam(required = false) List<TechStack> techStacks,
       @RequestParam(required = false) String employmentType,
       @RequestParam(required = false) Integer minCareer,
       @RequestParam(required = false) Integer maxCareer,
-      @RequestParam(required = false) Education education) {
+      @RequestParam(required = false) Education education,
+      @RequestParam(required = false) Long minSalary,
+      @RequestParam(required = false) Long maxSalary) {
 
     return searchService.searchJobPostings(position, techStacks, employmentType, minCareer,
-        maxCareer, education);
+        maxCareer, education, minSalary, maxSalary);
+  }
+
+  @GetMapping("/search")
+  public List<String> searchJobPostings(@RequestParam("keyword") String keyword) {
+    return searchService.searchJobPostingsByKeyword(keyword);
   }
 }
